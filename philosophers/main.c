9999ed72e8dcca63749358efd3f5c2e457ca9dfb
André Almeida <andre_rdo@hotmail.com>
2023-05-29 13:58:03 -0300

    Initial Commit

---
+++ philosophers/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  philosophers
+//
+//  Created by Andre Almeida on 29/05/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
